5d6ef8941d52400852b3d22e1d32e3afe591b3f1
Philipp Hug <philipp@hug.cx>
2017-09-26 16:37:01 +0200

    add initial jaeger tracing

--- tracing/config.go
+++ tracing/config.go
@@ -5,11 +5,46 @@ import (
 
 	"github.com/uber/jaeger-lib/metrics"
 
-	//"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	jaegerlog "github.com/uber/jaeger-client-go/log"
+
 )
 
+func InitGlobalTracer_testing() {
+	// Sample configuration for testing. Use constant sampling to sample every trace
+	// and enable LogSpan to log every span via configured Logger.
+	cfg := jaegercfg.Configuration{
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+
+	// Example logger and metrics factory. Use github.com/uber/jaeger-client-go/log
+	// and github.com/uber/jaeger-lib/metrics respectively to bind to real logging and metrics
+	// frameworks.
+	jLogger := jaegerlog.StdLogger
+	jMetricsFactory := metrics.NullFactory
+
+	// Initialize tracer with a logger and a metrics factory
+	_, err := cfg.InitGlobalTracer(
+		"jwtproxy",
+		jaegercfg.Logger(jLogger),
+		jaegercfg.Metrics(jMetricsFactory),
+	)
+	if err != nil {
+		log.Printf("Could not initialize jaeger tracer: %s", err.Error())
+		return
+	}
+	log.Printf("jaeger tracer initialized")
+	//defer closer.Close()
+
+	// continue main()
+}
 func InitGlobalTracer_production() {
 	// Recommended configuration for production.
 	cfg := jaegercfg.Configuration{}
@@ -21,7 +56,7 @@ func InitGlobalTracer_production() {
 	jMetricsFactory := metrics.NullFactory
 
 	// Initialize tracer with a logger and a metrics factory
-	closer, err := cfg.InitGlobalTracer(
+	_, err := cfg.InitGlobalTracer(
 		"jwtproxy",
 		jaegercfg.Logger(jLogger),
 		jaegercfg.Metrics(jMetricsFactory),
@@ -30,7 +65,8 @@ func InitGlobalTracer_production() {
 		log.Printf("Could not initialize jaeger tracer: %s", err.Error())
 		return
 	}
-	defer closer.Close()
+	log.Printf("jaeger tracer initialized")
+	//defer closer.Close()
 
 	// continue main()
 }
